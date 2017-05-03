ab2410de1a27741a95438d54b7484c722cb6b76e
Danny Hurlburt <danny.hurlburt@hypersoftllc.com>
2017-05-03 09:55:09 -0600

    Add the Code and its Tests

---
+++ src/index.test.ts
@@ -0,0 +1,215 @@
+
+import { typeOf, type_of } from './index';
+
+describe('qc-type_of', () => {
+
+  describe('`typeOf`', () => {
+
+    it('should be a function', () => {
+      expect(typeof typeOf).toBe('function');
+    });
+
+    it('called with an Arguments object should return `"arguments"`', function () {
+      let result = typeOf(arguments);
+      expect(result).toBe('arguments');
+    });
+
+    it('called with an empty array should return `"array"`', () => {
+      let result = typeOf([]);
+      expect(result).toBe('array');
+    });
+
+    it('called with `Array` object should return `"array"`', () => {
+      let result = typeOf(new Array('one', 'two', 'etc'));
+      expect(result).toBe('array');
+    });
+
+    it('called with `false` should return `"boolean"`', () => {
+      let result = typeOf(false);
+      expect(result).toBe('boolean');
+    });
+
+    it('called with `true` should return `"boolean"`', () => {
+      let result = typeOf(true);
+      expect(result).toBe('boolean');
+    });
+
+    it('called with `Boolean` object should return `"boolean"`', () => {
+      // Although it is bad practice to use the Boolean constructor, this confirms that 'object' is not returned.
+      let result = typeOf(new Boolean(true));
+      expect(result).toBe('boolean');
+    });
+
+    it('called with a date should return `"date"`', () => {
+      let result = typeOf(new Date());
+      expect(result).toBe('date');
+    });
+
+    it('called with an `Error` object should return `"object"`', () => {
+      let result = typeOf(new Error('Help!'));
+      expect(result).toBe('object');
+    });
+
+    it('called with a function should return `"function"`', () => {
+      let result = typeOf(function () {});
+      expect(result).toBe('function');
+
+      result = typeOf(Array);
+      expect(result).toBe('function');
+
+      result = typeOf(Boolean);
+      expect(result).toBe('function');
+
+      result = typeOf(Date);
+      expect(result).toBe('function');
+
+      result = typeOf(Error);
+      expect(result).toBe('function');
+
+      result = typeOf(Function);
+      expect(result).toBe('function');
+
+      result = typeOf(Number);
+      expect(result).toBe('function');
+
+      result = typeOf(Object);
+      expect(result).toBe('function');
+
+      result = typeOf(RegExp);
+      expect(result).toBe('function');
+
+      result = typeOf(String);
+      expect(result).toBe('function');
+    });
+
+    it('called with `Function` object should return `"function"`', () => {
+      let result = typeOf(new Function('return;'));
+      expect(result).toBe('function');
+    });
+
+    it('called with an empty object literal should return `"object"`', () => {
+      let result = typeOf({});
+      expect(result).toBe('object');
+    });
+
+    it('called with a non-empty object literal should return `"object"`', () => {
+      let result = typeOf({ prop: 'not empty' });
+      expect(result).toBe('object');
+    });
+
+    it('called with `null` should return `"null"`', () => {
+      let result = typeOf(null);
+      expect(result).toBe('null');
+    });
+
+    it('called with `Infinity` should return `"infinity"`', () => {
+      let result = typeOf(Infinity);
+      expect(result).toBe('infinity');
+    });
+
+    it('called with `Number.NEGATIVE_INFINITY` should return `"infinity"`', () => {
+      let result = typeOf(Number.NEGATIVE_INFINITY);
+      expect(result).toBe('infinity');
+    });
+
+    it('called with `Number.POSITIVE_INFINITY` should return `"infinity"`', () => {
+      let result = typeOf(Number.POSITIVE_INFINITY);
+      expect(result).toBe('infinity');
+    });
+
+    it('called with `NaN` should return `"nan"`', () => {
+      let result = typeOf(NaN);
+      expect(result).toBe('nan');
+    });
+
+    it('called with `Number.NaN` should return `"nan"`', () => {
+      let result = typeOf(Number.NaN);
+      expect(result).toBe('nan');
+    });
+
+    it('called with `Number.MIN_VALUE` should return `"number"`', () => {
+      let result = typeOf(Number.MIN_VALUE);
+      expect(result).toBe('number');
+    });
+
+    it('called with `-1` should return `"number"`', () => {
+      let result = typeOf(-1);
+      expect(result).toBe('number');
+    });
+
+    it('called with `0` should return `"number"`', () => {
+      let result = typeOf(0);
+      expect(result).toBe('number');
+    });
+
+    it('called with `1` should return `"number"`', () => {
+      let result = typeOf(1);
+      expect(result).toBe('number');
+    });
+
+    it('called with `Number.MAX_VALUE` should return `"number"`', () => {
+      let result = typeOf(Number.MAX_VALUE);
+      expect(result).toBe('number');
+    });
+
+    it('called with `Number` object should return `"number"`', () => {
+      let result = typeOf(new Number(3.14));
+      expect(result).toBe('number');
+    });
+
+    it('called with a regular expression should return `"regexp"`', () => {
+      let result = typeOf(/typeof/);
+      expect(result).toBe('regexp');
+    });
+
+    it('called with `RegExp` object should return `"regexp"`', () => {
+      let result = typeOf(new RegExp('.*\\..*'));
+      expect(result).toBe('regexp');
+    });
+
+    it('called with an empty string should return `"string"`', () => {
+      let result = typeOf('');
+      expect(result).toBe('string');
+    });
+
+    it('called with a non-empty string should return `"string"`', () => {
+      let result = typeOf('not empty');
+      expect(result).toBe('string');
+    });
+
+    it('called with `String` object should return `"string"`', () => {
+      // Although it is bad practice to use the String constructor, this confirms that 'object' is not returned.
+      let result = typeOf(new String('WTF'));
+      expect(result).toBe('string');
+    });
+
+    it('called with a `Symbol` object should return `"symbol"`', () => {
+      if (typeof Symbol == 'function' && typeof Symbol.iterator == 'symbol') {
+        let result = typeOf(Symbol.iterator);
+        expect(result).toBe('symbol');
+      }
+    });
+
+    it('called with `undefined` should return `"undefined"`', () => {
+      let result = typeOf(undefined);
+      expect(result).toBe('undefined');
+
+      result = typeOf();
+      expect(result).toBe('undefined');
+    });
+
+  });
+
+  describe('`type_of`', () => {
+
+    it('should be a function', () => {
+      expect(typeof type_of).toBe('function');
+    });
+
+    it('should be an alias of `typeOf`', () => {
+      expect(type_of).toBe(typeOf);
+    });
+
+  });
+
+});
---
+++ src/index.ts
@@ -0,0 +1,122 @@
+
+// ==========================================================================
+/**
+ * Returns the type of the specified value as a string.  The list of possible types are:
+ *
+ * - `'arguments'`: If the specified value is a function Arguments object.
+ * - `'array'`: If the specified value is an array.
+ * - `'boolean'`: If the specified value is a boolean value.
+ * - `'date'`: If the specified value is a `Date` object.
+ * - `'function'`: If the specified value is a function reference.  This includes generator functions.
+ * - `'infinity'`: If the specified value is `Infinity`, `Number.NEGATIVE_INFINITY`, or `Number.POSITIVE_INFINITY`.
+ * - `'nan'`: If the specified value is `NaN`.
+ * - `'null'`: If the specified value is `null`.
+ * - `'number'`: If the specified value is a number.
+ * - `'object'`: If the specified value is an object.
+ * - `'regexp'`: If the specified value is a regular expression.
+ * - `'string'`: If the specified value is a string.
+ * - `'symbol'`: If the specified value is a symbol (ES6).
+ * - `'undefined'`: If the specified value is `undefined`.
+ * - `'undetermined'`: If the specified value has an undetermined type.  If this ever happens, then update this function
+ *   to appropriately handle the value.
+ *
+ * **Example Usage:**
+ *
+ * ```js
+ * (function () {
+ *   typeOf(arguments);  // 'arguments'
+ * })();
+ *
+ * typeof [];  // 'object'
+ * typeOf([]);  // 'array' instead of 'object'
+ *
+ * typeOf(true);  // 'boolean'
+ *
+ * typeof new Date();  // 'object'
+ * typeOf(new Date());  // 'date' instead of 'object'
+ *
+ * typeOf(function () {});  // 'function'
+ *
+ * typeof Infinity;  // 'number'
+ * typeOf(Infinity);  // 'infinity' instead of 'number'
+ *
+ * typeof NaN;  // 'number'
+ * typeOf(NaN);  // 'nan' instead of 'number'
+ *
+ * typeof null;  // 'object'
+ * typeOf(null);  // 'null'
+ *
+ * typeOf(1234);  // 'number'
+ *
+ * typeOf({});  // 'object'
+ *
+ * typeof /regexp/;  // 'object'
+ * typeOf(/regexp/);  // 'regexp' instead of 'object'
+ *
+ * typeOf('');  // 'string'
+ *
+ * typeOf();  // 'undefined'
+ * ```
+ *
+ * @param {?*} value - The value to determine the type of.
+ *
+ * @return {string} The type of the specified value.
+ */
+function typeOf(value?: any): string {
+  /*
+   * Dependencies:
+   * - Core JS API.
+   */
+  let typeOfValue: string, typeToString: string;
+
+  if (value === null) {
+    return 'null';
+  }
+
+  typeOfValue = typeof value;
+
+  if (typeOfValue == 'undefined' || typeOfValue == 'string' || typeOfValue == 'boolean' || typeOfValue == 'function' || typeOfValue == 'symbol') {
+    return typeOfValue;
+  }
+
+  typeToString = Object.prototype.toString.call(value);
+
+  switch (typeToString) {
+  case '[object Arguments]':
+    return 'arguments';
+  case '[object Array]':
+    return 'array';
+  case '[object Date]':
+    return 'date';
+  case '[object Boolean]':
+    return 'boolean';
+  case '[object Number]':
+    if (value === Number.POSITIVE_INFINITY || value === Number.NEGATIVE_INFINITY) {
+      return 'infinity';
+    }
+    // The following removes the dependency on Number.isNaN which first showed up in ES6.
+    // The following works because `NaN` is the only value in JavaScript that does not equal itself.
+    if (value !== value) {
+      return 'nan';
+    }
+    return 'number';
+  case '[object RegExp]':
+    return 'regexp';
+  case '[object String]':
+    return 'string';
+  }
+
+  if (typeOfValue == 'object') {
+    return 'object';
+  }
+
+// @if DEBUG
+  /* eslint no-console: "off" */
+  console.warn(`Unable to determine type of "${value}".  Please update npm:qc-type_of:typeOf function to handle it.`);
+// @endif
+
+  return 'undetermined';
+}
+
+// ==========================================================================
+export { typeOf, typeOf as type_of };
